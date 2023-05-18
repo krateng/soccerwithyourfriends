5c7e90ba1b64c751eb7563e0d3d52f9eaf8ac5bd
krateng <git.noreply@krateng.ch>
2023-05-18 05:22:57 +0200

    Dynamic config loading

--- soccerwithyourfriends/config.py
+++ soccerwithyourfriends/config.py
@@ -1,29 +1,34 @@
+from .fileload import observe_file
+
 import toml
 
+config = {
+	'branding':{
+		'league_name': "Cool League",
+		'main_font': "font-thin.otf",
+		'main_font_bold': "font-regular.otf",
+		'league_font': "coolfont.ttf",
+		'logo': "league.png",
+		'colors': ["red","white","darkgrey","grey"]
+	}
+}
 
-defaultconfig = """[branding]
 
-# These are all relative to the 'branding' folder
-league_name = "Cool League"
-main_font = "font-thin.otf"
-main_font_bold = "font-regular.otf"
-league_font = "coolfont.ttf"
-logo = "league.png"
-colors = ["red","white","darkgray","lightgrey"]
+def set_default_config(srcfile):
+	try:
+		loadedconfig = toml.load(srcfile)
+	except FileNotFoundError:
+		loadedconfig = {}
 
-[current]
+	config.update(loadedconfig)
+	if config != loadedconfig:
+		with open(srcfile,'w') as fd:
+			toml.dump(config,fd)
 
-season = 1
+def load_config(srcfile):
 
-# You also need the following folders in your data folder:
-teams		For badges / logos
-newsimages	Story images
-import		Data that is added to the DB at launch
-"""
+	global config
+	config.update(toml.load(srcfile))
 
-try:
-	config = toml.load('config.toml')
-except FileNotFoundError:
-	with open('config.toml','w') as tomlfile:
-		tomlfile.write(defaultconfig)
-	config = toml.load('config.toml')
+set_default_config('config.toml')
+observe_file('config.toml',load_config)
